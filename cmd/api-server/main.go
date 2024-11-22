168f6994bd89c42262f6042a6ecd418e21afeb1c
unknown <avindu@wso2.com>
2024-11-22 15:39:47 +0530

    Completed router.go and updated main.go with router

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"newsAPI/cmd/api-server/internal/router"
 )
 
 func main() {
 
+	r := router.New()
 	//Building a HTTP server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 
 		log.Fatal("Failed to start server ", err)
 
